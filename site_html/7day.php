ac1b3e7c2967113f3448f01764ccc250df720466
dfellowstwo <dfellows8@juno.com>
2022-03-10 18:32:14 -0600

    weather

---
+++ site_html/7day.php
@@ -0,0 +1,2 @@
+document.write('<a href=https://www.dfellows.rf.gd/site_html/weather-seven-day-forecast-graphic.jpg><img src="https://www.dfellows.rf.gd/site_html/weather-seven-day-forecast-graphic.jpg" width="480" height="196" alt="Click to load the 7 day forecast graphic." /></a>');
+
