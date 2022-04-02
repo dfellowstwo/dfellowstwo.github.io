9847a16b468de32b3e6a4fc844f13f58623655c0
dfellowstwo <dfellows8@juno.com>
2022-04-02 18:15:07 -0500

    weather

--- site_html/7day.php
+++
@@ -1,5 +0,0 @@
-document.write('<a href=#><img src="https://www.dfellows.rf.gd/site_html/weather-seven-day-forecast-graphic.jpg" width="480" height="196" alt="Click to load the 7 day forecast graphic." title="Click to load the 7 day forecast graphic." /></a>');
-
-document.write('<img src="https://www.dfellows.rf.gd/site_html/weather-seven-day-forecast-graphic.jpg"  width="480" height="196" alt="Click to load the 7 day forecast graphic." title="Click to load the 7 day forecast graphic." />');
-
-document.write('<a href=https://www.dfellows.rf.gd/sitehtml/weather-seven-day-forecast-graphic.jpg><img src="https://www.dfellows.rf.gd/sitehtml/weather-seven-day-forecast-graphic.jpg"  width="480" height="196" alt="Click to load the 7 day forecast graphic." title="Click to load the 7 day forecast graphic." /></a>');
